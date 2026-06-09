f267288966afe7ffd760b3c6ef2ac777ea21cbb2
Cory McLean <s3707892@student.rmit.edu.au>
2026-06-09 21:54:53 +1000

    Update Client.ts

--- src/api/Client.ts
+++ src/api/Client.ts
@@ -1,10 +1,10 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || 'https://fsd-a2-6gnarblof-cory-s-projects7.vercel.app',
+  baseURL: process.env.REACT_APP_API_URL || 'https://a2-fsd-pra01-02-wed-6-30pm-alex-team-24-v893.onrender.com',
 });
 
-// Attach JWT token to every request if one is stored
+// Attach JWT token to every request if one is storedd
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('vv_token');
   if (token) {
